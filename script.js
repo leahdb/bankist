5ef8549611c6c2ad7d4153ef3e9ebbf2446629b4
leahdb <hdeiblea72@gmail.com>
2022-09-19 13:01:51 +0300

    logout and delete the account

--- script.js
+++ script.js
@@ -200,3 +200,24 @@ btnLoan.addEventListener("click", function (e) {
   }
   inputLoanAmount.value = "";
 });
+
+
+btnClose.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      (acc) => acc.username === currentAccount.username
+    );
+
+    accounts.splice(index, 1);   //delete the account
+
+    containerApp.style.opacity = 0;
+    labelWelcome.textContent = "Log in to get started";
+  }
+
+  inputCloseUsername.value = inputClosePin.value = "";
+});
